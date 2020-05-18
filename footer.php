4241da15e6d63f02d0e81d7bff288ff278ecb4af
Deeksha Vashisht <45069193+DeekshaV03@users.noreply.github.com>
2020-05-18 21:33:57 +0530

    Geo Voyage Initial

---
+++ footer.php
@@ -0,0 +1,56 @@
+<footer id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-center bottom-separator">
+                    <img src="images/home/under.png" class="img-responsive inline" alt="">
+                </div>
+                <div class="col-md-4 col-sm-6" style="padding-left: 150px;">
+                    <div class="testimonial bottom">
+                        <h2>Quick Links</h2>
+                        <p><a href="index.html"><b>Home</b></a></p>
+                        <p><a href="Climate.html">Climate</a></p>
+                        <p><a href="Disasters.html">Disaster</a></p>
+                        <p><a href="Travel Diaries.html">Travel Diaries</a></p>
+                        <p><a href="Blog.html">Blog</a></p> 
+                    </div> 
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="contact-info bottom">
+                        <h2>Contacts</h2>
+                        <address>
+                        E-mail: <a href="mailto:geovoyage@gmail.com">geovoyage@gmail.com</a> <br> 
+                        Phone: +1 (123) 456 7890 <br> 
+                        </address>
+                        <h2>Address</h2>
+                        <address>
+                        New Delhi, India <br> 
+                        </address>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-12">
+                    <div class="contact-form bottom">
+                        <h2>Send a message</h2>
+                        <form id="main-contact-form" name="contact-form" method="post" action="sendemail.php">
+                            <div class="form-group">
+                                <input type="text" name="name" class="form-control" required="required" placeholder="Name">
+                            </div>
+                            <div class="form-group">
+                                <input type="email" name="email" class="form-control" required="required" placeholder="Email Id">
+                            </div>
+                            <div class="form-group">
+                                <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Your text here"></textarea>
+                            </div>                        
+                            <div class="form-group">
+                                <input type="submit" name="submit" class="btn btn-submit" value="Submit">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="copyright-text text-center">
+                        <p>&copy; Geo Voyage 2020. All Rights Reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </div>        
+    </footer>
